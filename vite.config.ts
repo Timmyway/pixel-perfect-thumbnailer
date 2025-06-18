a29d598ae5c26f30102d255871f2e8eeb4ba4ca4
Tim <timmyway1990@gmail.com>
2025-06-18 15:24:28 +0300

    feat(config): load .env variables using Vite's loadEnv and set base path dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? (process.env.VITE_BASE_PATH || '/') : '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // 👈 charge les variables .env
+  console.log('========>', env.VITE_BASE_PATH)
+
+  return {
+    base: mode === 'production' ? (env.VITE_BASE_PATH || '/') : '/',
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
+});
